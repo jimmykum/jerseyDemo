ff7b056b32688748779ea6785d559004e158537d
unknown <jimmy.kumar@gmail.com>
2016-06-07 21:35:29 +0530

    first commit

---
+++ RESTfulExample/src/main/java/com/service/Todo.java
@@ -0,0 +1,27 @@
+package com.service;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
+public class Todo {
+
+	private String summary;
+	  private String description;
+	public String getSummary() {
+		return summary;
+	}
+	public void setSummary(String summary) {
+		this.summary = summary;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	@Override
+	public String toString() {
+		return "Todo [summary=" + summary + ", description=" + description
+				+ "]";
+	}
+}
